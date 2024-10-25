d01f2582ef43c527c5049f4d7bdfd3a75c4d5464
Ivan Tikhonov <itikhonov@mail.com>
2024-10-25 17:12:57 +0300

    add tests

---
+++ IT.Utf8String.Tests/JsonTest.cs
@@ -0,0 +1,40 @@
+﻿using IT;
+using System.Text.Json;
+
+namespace UserSigning.Tests;
+
+public class JsonTest
+{
+    record Entity
+    {
+        public string? Str { get; set; }
+
+        public Utf8String? Utf8Str { get; set; }
+    }
+
+    [Test]
+    public void SerializeTest()
+    {
+        var obj = new Entity() { Str = "my utf8 str", Utf8Str = "my utf8 str"u8.ToArray() };
+        var json = JsonSerializer.Serialize(obj);
+
+        Assert.That(json, Is.EqualTo("{\"Str\":\"my utf8 str\",\"Utf8Str\":\"my utf8 str\"}"));
+        Assert.That(JsonSerializer.Deserialize<Entity>(json), Is.EqualTo(obj));
+
+        obj = new Entity();
+        json = JsonSerializer.Serialize(obj);
+
+        Assert.That(json, Is.EqualTo("{\"Str\":null,\"Utf8Str\":null}"));
+        Assert.That(JsonSerializer.Deserialize<Entity>(json), Is.EqualTo(obj));
+
+        obj = new Entity() { Str = "моя utf8 строка", Utf8Str = "моя utf8 строка"u8.ToArray() };
+        json = JsonSerializer.Serialize(obj);
+
+        Assert.That(json, Is.EqualTo("{\"Str\":\"\\u043C\\u043E\\u044F utf8 \\u0441\\u0442\\u0440\\u043E\\u043A\\u0430\",\"Utf8Str\":\"\\u043C\\u043E\\u044F utf8 \\u0441\\u0442\\u0440\\u043E\\u043A\\u0430\"}"));
+        Assert.That(JsonSerializer.Deserialize<Entity>(json), Is.Not.EqualTo(obj));
+
+        json = "{\"Str\":\"\\u043C\\u043E\\u044F utf8 \\u0441\\u0442\\u0440\\u043E\\u043A\\u0430\",\"Utf8Str\":\"моя utf8 строка\"}";
+
+        Assert.That(JsonSerializer.Deserialize<Entity>(json), Is.EqualTo(obj));
+    }
+}

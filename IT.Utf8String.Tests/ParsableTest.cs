a9f3b63baccfbbae1bbf1818e53c76d07497e028
Ivan Tikhonov <itikhonov@mail.com>
2024-10-26 19:59:52 +0300

    ParsableTest

---
+++ IT.Utf8String.Tests/ParsableTest.cs
@@ -0,0 +1,18 @@
+﻿using IT;
+
+namespace Tests;
+
+public class ParsableTest
+{
+    [Test]
+    public void ISpanFormattable_Test()
+    {
+        var chars = "моя строка".ToArray();
+        var bytes = "моя строка"u8.ToArray();
+
+        Assert.That(Utf8String.Parse(chars), Is.EqualTo(new Utf8String(bytes)));
+
+        Assert.That(Utf8String.TryParse(chars, out var utf8String), Is.True);
+        Assert.That(utf8String, Is.EqualTo(new Utf8String(bytes)));
+    }
+}

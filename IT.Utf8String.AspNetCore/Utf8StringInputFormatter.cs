ae3ae5a2d116ec04be7dc0a0ff05e57fb27c7e38
Ivan Tikhonov <itikhonov@mail.com>
2024-11-01 17:13:29 +0300

    IT.Utf8String.AspNetCore

---
+++ IT.Utf8String.AspNetCore/Utf8StringInputFormatter.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+
+namespace IT.AspNetCore.Mvc.Formatters;
+
+public class Utf8StringInputFormatter : InputFormatter
+{
+    private static readonly MediaTypeHeaderValue _mediaType = new("text/plain") { Charset = "utf-8" };
+
+    public Utf8StringInputFormatter()
+    {
+        SupportedMediaTypes.Add(_mediaType);
+    }
+
+    protected override bool CanReadType(Type type) => type == typeof(Utf8String) || type == typeof(ReadOnlyUtf8String);
+
+    public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+    {
+        var request = context.HttpContext.Request;
+
+        var length = request.ContentLength ?? throw new InvalidOperationException("ContentLength is null");
+        if (length > int.MaxValue) throw new InvalidOperationException("ContentLength is too large");
+
+        var bytes = await ReadAsync(length, request.Body);
+
+        if (bytes != null)
+        {
+            var modelType = context.ModelType;
+            if (modelType == typeof(Utf8String)) return InputFormatterResult.Success(new Utf8String(bytes));
+            if (modelType == typeof(ReadOnlyUtf8String)) return InputFormatterResult.Success(new ReadOnlyUtf8String(bytes));
+        }
+
+        return InputFormatterResult.Failure();
+    }
+
+    private static async Task<byte[]> ReadAsync(long length, Stream stream)
+    {
+        var bytes = new byte[length];
+        var memory = bytes.AsMemory();
+        int readed;
+
+        do
+        {
+            readed = await stream.ReadAsync(memory);
+
+            memory = memory[readed..];
+
+        } while (memory.Length > 0);
+
+        return bytes;
+    }
+}
---
+++ IT.Utf8String.AspNetCore/Utf8StringOutputFormatter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System.Buffers;
+
+namespace IT.AspNetCore.Mvc.Formatters;
+
+public class Utf8StringOutputFormatter : OutputFormatter
+{
+    private static readonly MediaTypeHeaderValue _mediaType = new("text/plain") { Charset = "utf-8" };
+
+    public Utf8StringOutputFormatter()
+    {
+        SupportedMediaTypes.Add(_mediaType);
+    }
+
+    protected override bool CanWriteType(Type? type) => type == typeof(Utf8String) || type == typeof(ReadOnlyUtf8String);
+
+    public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context)
+    {
+        var obj = context.Object;
+
+        var writer = context.HttpContext.Response.BodyWriter;
+
+        if (obj is Utf8String utf8String)
+        {
+            writer.Write(utf8String.Span);
+        }
+        else if (obj is ReadOnlyUtf8String readOnlyUtf8String)
+        {
+            writer.Write(readOnlyUtf8String.Span);
+        }
+        else
+        {
+            throw new NotSupportedException(obj != null ? $"Type {obj.GetType()} not supported" : "Object is null");
+        }
+
+        return writer.FlushAsync().AsTask();
+    }
+}

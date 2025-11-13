045b6096d472063664fe4324b3691f77e200297e
Ivan Tikhonov <itikhonov@mail.com>
2025-11-13 16:22:24 +0300

    ref

--- IT.Utf8String/Utf8String.cs
+++ IT.Utf8String/Utf8String.cs
@@ -321,7 +321,7 @@ public readonly struct Utf8String : IComparable<Utf8String>, IEquatable<Utf8Stri
 #if NET7_0_OR_GREATER
 
     static Utf8String ISpanParsable<Utf8String>.Parse(ReadOnlySpan<char> s, IFormatProvider? provider)
-    => Parse(s);
+        => Parse(s);
 
     static bool ISpanParsable<Utf8String>.TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, [System.Diagnostics.CodeAnalysis.MaybeNullWhen(false)] out Utf8String result)
         => TryParse(s, out result);
